ad6bd081db24a6248be2d4da847cfe5bb6555fcf
waterdisappear <40572063+waterdisappear@users.noreply.github.com>
2025-05-06 12:20:02 +0800

    Update DataLoad.py

--- ATRBench/Classification/ConvNeXt/utils/DataLoad.py
+++ ATRBench/Classification/ConvNeXt/utils/DataLoad.py
@@ -35,10 +35,16 @@ from torch.utils import data
 import numpy as np
 from PIL import Image
 
+# data_transform = transforms.Compose([
+#     transforms.Resize(224), # 缩放到 96 * 96 大小
+#     transforms.ToTensor(),
+#     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+# ])
+
 data_transform = transforms.Compose([
-    transforms.Resize(224), # 缩放到 96 * 96 大小
+    # transforms.Resize(224), # 缩放到 96 * 96 大小
     transforms.ToTensor(),
-    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
 class custom_dset(Dataset):
